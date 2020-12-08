04b7158edbecc824c599204371fdf3e1c01ad15e
swapnanildutta <swapnanildutta2000@gmail.com>
2020-12-09 01:05:51 +0530

    GET and POST requests 📫📮

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html', name='Swapnanil')
+    return render_template('home.html')
 
-@app.route('/about')
-def about():
-    return 'This is a URL Shortener'
+@app.route('/your-url', methods=['GET', 'POST'])
+def your_url():
+    if request.method == 'POST':
+        return render_template('your_url.html', code=request.form['code'])
+    else:
+        return 'This is not allowed'
+
