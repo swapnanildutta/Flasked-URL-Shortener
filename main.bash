531756ceed506b888f4f5853b716dbe35ad436a2
swapnanildutta <swapnanildutta2000@gmail.com>
2020-12-13 16:50:53 +0530

    UI, Testing, and lots 🤯🤯🤯🤯

--- main.bash
+++ main.bash
@@ -3,6 +3,8 @@
 #*pipenv install
 pipenv shell
 #*pipenv install flask
+#*pipenv install pytest
 #*export FLASK_APP=hello
 export FLASK_ENV=development
+export FLASK_APP=urlshort
 flask run
--- urlshort/urlshort.py
+++ urlshort/urlshort.py
@@ -1,16 +1,15 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, abort, session
+from flask import render_template, request, redirect, url_for, flash, abort, session, jsonify, Blueprint
 import json
 import os.path
 from werkzeug.utils import secure_filename
 
-app = Flask(__name__)
-app.secret_key = '458645412364'
+bp = Blueprint('urlshort', __name__)
 
-@app.route('/')
+@bp.route('/')
 def home():
     return render_template('home.html', codes=session.keys())
 
-@app.route('/your-url', methods=['GET', 'POST'])
+@bp.route('/your-url', methods=['GET', 'POST'])
 def your_url():
     if request.method == 'POST':
         urls ={}
@@ -21,7 +20,7 @@ def your_url():
         #Check for conflicts
         if request.form['code'] in urls.keys():
             flash('That short name is already taken, please choose another name.')
-            return redirect(url_for('home'))
+            return redirect(url_for('urlshort.home'))
 
         #Check if url is given
         if 'url' in request.form.keys():
@@ -30,7 +29,7 @@ def your_url():
         else:
             f = request.files['file']
             full_name = request.form['code'] + secure_filename(f.filename)
-            f.save('/home/swapnanildutta/Desktop/d/Flasked-URL-Shortener/static/user_files/' + full_name)
+            f.save('/home/swapnanildutta/Desktop/d/Flasked-URL-Shortener/urlshort/static/user_files/' + full_name)
             urls[request.form['code']] = { 'file' : full_name }
 
         #Append all contents to JSON Directory
@@ -39,10 +38,10 @@ def your_url():
             session[request.form['code']] = True
         return render_template('your_url.html', code=request.form['code'])
     else:
-        return redirect(url_for('home'))
+        return redirect(url_for('urlshort.home'))
 
 #Access shortened content
-@app.route('/<string:code>')
+@bp.route('/<string:code>')
 def redirect_to_url(code):
     #Check if file exists
     if os.path.exists('urls.json'):
@@ -58,6 +57,10 @@ def redirect_to_url(code):
     #Error handling
     return abort(404)
 
-@app.errorhandler(404)
+@bp.errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html'), 404
+
+@bp.route('/api')
+def session_api():
+    return jsonify(list(session.keys()))
